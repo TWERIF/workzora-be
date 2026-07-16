587cc045fefcccc8a3f2a283a045ee4baa20091a
DanyloUserGit <danylom2007.m@gmail.com>
2026-07-16 20:37:52 +0300

    16.07.2026

--- admin-panel/src/shared/http/index.ts
+++ admin-panel/src/shared/http/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000', //`http://localhost:8000`
+  baseURL: `https://workzora.com/api`, //`http://localhost:8000`
   timeout: 10000,
-  withCredentials:true
+  withCredentials: true
 });
