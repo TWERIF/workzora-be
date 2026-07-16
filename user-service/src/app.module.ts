6273acc3730b4bce2bd8c0f4e510acd27ce21e74
DanyloUserGit <danylom2007.m@gmail.com>
2026-07-16 21:13:06 +0300

    16.07.2026

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,23 +1,43 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST || 'postgres',
-      port: Number(process.env.POSTGRES_PORT) || 5432,
-      username: process.env.POSTGRES_USER || 'workzorauser',
-      password: process.env.POSTGRES_PASSWORD || 'J95jACtFtadE',
-      database: process.env.POSTGRES_DB || 'workzoradb',
-      schema: 'users',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST || 'postgres',
+        port: Number(process.env.POSTGRES_PORT) || 5432,
+        username: process.env.POSTGRES_USER || 'workzorauser',
+        password: process.env.POSTGRES_PASSWORD || 'J95jACtFtadE',
+        database: process.env.POSTGRES_DB || 'workzoradb',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      dataSourceFactory: async (options) => {
+        if (!options) throw new Error('Invalid options passed');
+
+        const initDataSource = new DataSource({
+          ...options,
+          synchronize: false,
+        });
+
+        await initDataSource.initialize();
+
+        const schemas = ['users'];
+        for (const schema of schemas) {
+          await initDataSource.query(`CREATE SCHEMA IF NOT EXISTS "${schema}"`);
+        }
+
+        await initDataSource.destroy();
+
+        const dataSource = new DataSource(options);
+        return await dataSource.initialize();
+      },
     }),
-    UsersModule,
+    PostsModule,
   ],
-  controllers: [],
-  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
