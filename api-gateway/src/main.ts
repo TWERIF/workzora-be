b689ec4e606b73d90b75496702656ce09a1cd39f
DanyloUserGit <danylom2007.m@gmail.com>
2026-07-18 01:47:44 +0300

    18.07.2026

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3000', 'http://localhost:3001', 'https://workzora.com', 'https://workzora.com/admin', 'http://workzora.com/admin', 'http://185.229.251.118','http://185.229.251.118:3000','http://185.229.251.118:3001', 'http://workzora.com'], credentials: true });
+  app.enableCors({ origin: ['http://localhost:3000', 'http://localhost:3001', 'https://workzora.com', 'https://www.workzora.com', 'https://workzora.com/admin', 'http://workzora.com/admin', 'http://185.229.251.118','http://185.229.251.118:3000','http://185.229.251.118:3001', 'http://workzora.com'], credentials: true });
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 8000);
 }
